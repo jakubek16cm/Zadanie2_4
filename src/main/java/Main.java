79fabb80481ce7c90cc27ac6c6ce2d2378f33534
jakubek16cm <kuba.stanczyk201@gmail.com>
2025-03-22 10:42:05 +0000

    2_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,59 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("1 - Oblicz pole trójkąta prostokątngo");
+    System.out.println("2 - Sprawdź czy trójkąt jest prostokątny");
+    System.out.println(" ");
+    int choice = scanner.nextInt();
+
+  switch(choice){
+    case 1:
+      System.out.println("Podaj długość podstawy: ");
+      double base = scanner.nextDouble();
+
+      System.out.println("Podaj długość wysokości:  ");
+      double height = scanner.nextDouble();
+
+      double area = (base * height)/2;
+      System.out.println("Pole trójkąta wynosi: " +area);
+      break;
+
+
+    case 2:
+      System.out.print("Podaj długość boku a:  ");
+      double a = scanner.nextDouble();
+
+      System.out.println("Podaj długość boku b: ");
+      double b = scanner.nextDouble();
+
+      System.out.println("Podaj długość boku c: ");
+      double c = scanner.nextDouble();
+
+       if(a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b)
+       {
+      
+      System.out.println("Podany trójkąt jest prostokątny");
+       }
+        else
+      {
+      System.out.println("Podany trójkąt nie jest prostokątny");
+      }
+      break;
+      
+}
+        
+
+      
+      
+  
+    
+    
   }
 
   // @Test
